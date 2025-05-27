783819f6deca5b1e382319291a8ef07b2859c810
Emanuel Dario Garcia <emanueldario.dev@gmail.com>
2025-05-27 20:39:04 +0200

    auth configuration forgot and commited

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { dark } from '@clerk/themes'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), clerk({
+      appearance: {
+        baseTheme: [dark],
+      },
+  })],
 
+  adapter: node({ mode: "standalone" }),
+  output: "server",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
